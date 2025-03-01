16124fa1ec6789bd084044a6e331b98061890952
Safiro Alfarisi Haraya <sfralfarisi@gmail.com>
2025-03-01 23:13:27 +0700

    Implementasi query builder

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
@@ -26,6 +27,7 @@ route::get('/level', [LevelController::class, 'index']);
 Route::prefix('products')->group(function () {
     Route::get('/', [ProductController::class, 'index']);
 });
+Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/category', [CategoryController::class, 'index']);
 Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
 Route::get('/sales', [SalesController::class, 'index']);
