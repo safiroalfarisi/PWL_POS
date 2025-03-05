bf11810a3876de231d089bd4ed71e6314f9e0dbe
Safiro Alfarisi Haraya <sfralfarisi@gmail.com>
2025-03-05 10:12:21 +0700

    Implementasi Retreiving Aggregrates

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,16 +7,10 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <td>ID</td>
-            <td>Username</td>
-            <td>Nama</td>
-            <td>ID Level Pengguna</td>
+            <td><strong>Jumlah Pengguna</strong></td>
         </tr>
         <tr>
-            <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
-            <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td>
+            <td>{{ $data }}</td>
         </tr>
     </table>
 </body>
