45d613a7c5caa409cb4c1e3d24771ea357fa57b0
Safiro Alfarisi Haraya <sfralfarisi@gmail.com>
2025-04-22 21:07:26 +0700

    Implmentasi fitur import dan export level

--- resources/views/level/export_pdf.blade.php
+++ resources/views/level/export_pdf.blade.php
@@ -29,9 +29,9 @@
 
         img.image {
             width: auto;
-            height: 50px;
-            max-width: 100px;
-            max-height: 100px;
+            height: 80px;
+            max-width: 150px;
+            max-height: 150px;
         }
 
         .text-right {
@@ -77,7 +77,7 @@
 <body>
     <table class="border-bottom-header">
         <tr>
-            <td width="15%" class="text-center"><img src="{{ asset('polinema-bw.png') }}" class="image" style="height: 150px; max-width: 100px;"></td>
+            <td width="15%" class="text-center"><img src="{{ asset('polinema-bw.png') }}"></td>
             <td width="85%">
                 <span class="text-center d-block font-11 font-bold mb-1">KEMENTERIAN
                     PENDIDIKAN, KEBUDAYAAN, RISET, DAN TEKNOLOGI</span>
@@ -91,27 +91,21 @@
             </td>
         </tr>
     </table>
-    <h3 class="text-center">LAPORAN DATA BARANG</h4>
+    <h3 class="text-center">LAPORAN DATA LEVEL</h4>
         <table class="border-all">
             <thead>
                 <tr>
                     <th class="text-center">No</th>
-                    <th>Kode Barang</th>
-                    <th>Nama Barang</th>
-                    <th class="text-right">Harga Beli</th>
-                    <th class="text-right">Harga Jual</th>
-                    <th>Kategori</th>
+                    <th>Kode Level</th>
+                    <th>Nama Level</th>
                 </tr>
             </thead>
             <tbody>
-                @foreach($barang as $b)
+                @foreach($level as $l)
                     <tr>
                         <td class="text-center">{{ $loop->iteration }}</td>
-                        <td>{{ $b->barang_kode }}</td>
-                        <td>{{ $b->barang_nama }}</td>
-                        <td class="text-right">{{ number_format($b->harga_beli, 0, ',', '.') }}</td>
-                        <td class="text-right">{{ number_format($b->harga_jual, 0, ',', '.') }}</td>
-                        <td>{{ $b->kategori->kategori_nama }}</td>
+                        <td>{{ $l->level_kode }}</td>
+                        <td>{{ $l->level_nama }}</td>
                     </tr>
                 @endforeach
             </tbody>
