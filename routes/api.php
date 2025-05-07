68cff8a7f188aeb5680dd1f6d5486b1ad8c2ccb5
Safiro Alfarisi Haraya <sfralfarisi@gmail.com>
2025-05-07 07:48:46 +0700

    praktikum 3

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,4 @@ Route::post('/login', LoginController::class)->name('login');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/logout', LogoutController::class)->name('logout');
